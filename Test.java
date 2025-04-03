5d30cdc635d5e7e35fbb9e78626a2d1defc1e214
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-04-03 12:45:30 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+package in.abc.dynamicinput;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		java.util.Date uDate = new java.util.Date();
+		System.out.println("UtilDate: " + uDate);
+
+		long l = uDate.getTime();
+		
+		java.sql.Date sqlDate = new java.sql.Date(l);
+		System.out.println("SQLDate : " + sqlDate);
+
+	}
+
+}
